8208326d907b0000f9ae55074454b14b7678e041
EEltigani <emadeltigani5@gmail.com>
2025-05-19 12:33:51 +0100

    Delete Start Form.vb

--- Start Form.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Start_Form
-
-    Private Sub btnStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStart.Click
-        Menu_Form.Show() 'shows the main menu
-    End Sub
-End Class
