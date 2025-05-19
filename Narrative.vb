dd932e1ae6c37058474d0b2dd2da45b51a9a23ad
EEltigani <emadeltigani5@gmail.com>
2025-05-19 12:24:44 +0100

    Add files via upload

---
+++ Narrative.vb
@@ -0,0 +1,14 @@
+﻿Public Class Narrative
+
+    Private Sub Narrative_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub btnQuit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnQuit.Click
+        Me.Close() 'to close the narrative form
+    End Sub
+
+    Private Sub btnMenu_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMenu.Click
+        Me.Close() 'to close the following menu form before displaying the instructions form
+        Menu_Form.Show() 'to proceed to the menu form
+    End Sub
+End Class
